24bd2f78d8358a5ebb93a72f8b3af4857d29cf8a
Krishna Krishnamaneni <kkrishnamaneni@ypcnb01839.yellowpages.local>
2009-02-02 17:53:23 -0800

    Saving files to appropriate model's files directory if there are any attributes that are files (useful in case of uploaded content)

--- lib/drivers/yaml.rb
+++ lib/drivers/yaml.rb
@@ -1,35 +1,47 @@
 require 'yaml'
 require 'extensions/io'
 class Filebase
-  
+
   class YAML
-    
+
     def initialize( root )
       @root = root.to_s
     end
-    
-  	def path( key )
-  	  File.join( @root, key.to_s ) + '.yml'
-  	end
-  	
-  	def all
-  	  Dir[ path('*') ].map { |file| find( File.basename(file,'.yml' ) ) }.compact
-  	end
-    
+
+    def path( key )
+      File.join( @root, key.to_s ) + '.yml'
+    end
+
+    def all
+      Dir[ path('*') ].map { |file| find( File.basename(file,'.yml' ) ) }.compact
+    end
+
     def find( key )
-  		obj = ::YAML.load( File.read( path( key ) ) ) rescue nil
-  		obj['key'] = key if Hash === obj
-  		obj or nil # convert false to nil
-  	end
-  	
-  	def save( key, object )
-  		File.write( path( key ), object.to_yaml ) and object
-  	end
-  	
-  	def delete( key )
-  		FileUtils.remove( path( key ) )
-	  end
-	  
-	end
+      obj = ::YAML.load( File.read( path( key ) ) ) rescue nil
+      obj['key'] = key if Hash === obj
+      obj or nil # convert false to nil
+    end
+
+    def save( key, object )
+      #Hack to save uploaded files before serialization.
+      object.each do |k, v|
+        if((v.class == Hash) and (v[:tempfile].class == Tempfile))
+          dirpath = File.join instance_variable_get('@root'), 'files'
+          ::FileUtils.mkdir_p(filepath) unless File.exists?(filepath)
+          filepath = File.join dirpath, key
+          ::FileUtils.mv(v[:tempfile].path, filepath, :force => true)
+          object['filepath']  =  filepath
+        end
+
+      end
+
+      File.write( path( key ), object.to_yaml ) and object
+    end
+
+    def delete( key )
+      FileUtils.remove( path( key ) )
+    end
+
+  end
 
 end
